3ea654475afef50f7171b28ba0b09d98abd5fbbd
Joao <jvfr96@gmail.com>
2023-11-01 15:45:42 -0300

    feat: ensure only latest treasury exchange is received

--- application/treasury/treasury.go
+++ application/treasury/treasury.go
@@ -23,7 +23,7 @@ func (te *TreasuryExchange) GetCountryExchange(country string, transactionDate t
 	var data dtos.ExchangeResponseDto
 	formattedLimitDate := transactionDate.AddDate(0, -6, 0).Format("2006-01-02")
 	formattedDate := transactionDate.Format("2006-01-02")
-	requestUrl := te.baseUrl + fmt.Sprintf("?filter=country:in:(%s),record_date:lte:%s,record_date:gte:%s&sort=-record_date", country, formattedDate, formattedLimitDate)
+	requestUrl := te.baseUrl + fmt.Sprintf("?filter=country:in:(%s),record_date:lte:%s,record_date:gte:%s&sort=-record_date&page[number]=1&page[size]=1", country, formattedDate, formattedLimitDate)
 	response, err := http.Get(requestUrl)
 	if err != nil {
 		return data, err
