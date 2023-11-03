a7157086582672a9a45202bfbbd3df68322d7b55
Joao <jvfr96@gmail.com>
2023-11-03 13:07:32 -0300

    feat: get port from env

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 
 	"github.com/jvfrodrigues/transaction-product-wex/application/api"
 	"github.com/jvfrodrigues/transaction-product-wex/infra/db"
+	"github.com/jvfrodrigues/transaction-product-wex/infra/logger/zap"
 )
 
 func main() {
+	logger := zap.NewLogger()
 	database := db.ConnectDB(os.Getenv("env"))
 	server := api.NewServer("release")
-	server.StartServer(database, ":8080")
+	port := preparePort()
+	logger.Info("Server running on " + port)
+	server.StartServer(database, port)
+}
+
+func preparePort() string {
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "8080"
+	}
+	serverPort := ":" + port
+	return serverPort
 }
